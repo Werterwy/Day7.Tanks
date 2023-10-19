1368e24ea6fa45ed66f79cb414da52bbe6e0a4dc
Orazbay <you@biy.kz>
2023-10-19 18:46:16 +0600

    Добавьте файлы проекта.

---
+++ Day7.Tanks/Program.cs
@@ -0,0 +1,44 @@
+﻿using MyClassLib.WordOfTanks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7.Tanks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] T34 = new Tank[5];
+            Tank[] Pantera= new Tank[5];    
+            for(int j=0; j<T34.Length; j++)
+            {
+                Tank t34 = new Tank("T34-"+ string.Format("{0}", j+1));
+                Tank pantera = new Tank("Pantera-" + string.Format("{0}", j + 1));
+                T34[j] = t34;
+                Pantera[j] = pantera;
+            }
+            Console.WriteLine("В танковом бою участвуют 5 танков «Т-34» и 5 танков «Pantera».\n  " +
+                "Каждый танк участвует в парной битве, т.е. первый танк «Т-34» \nсражается с первым танком «Pantera» и т. д. " +
+                "Победа присуждается тому танку,\n который превышает противника по двум и более параметрам из трех");
+            Console.WriteLine("");
+            Console.WriteLine("Сражение танков:");
+            for (int i = 0; i < T34.Length; i++)
+            {
+                Tank winner = T34[i] * Pantera[i];
+
+                if (winner != null)
+                {
+                    Console.WriteLine($"{winner.NameTank} победил в битве!");
+                }
+                else
+                {
+                    Console.WriteLine("Ничья в битве.");
+                }
+            }
+
+        }
+    }
+}
---
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        public string NameTank { get; set; }
+        private int TankAmmunition { get; set; }
+        private int ArmorLevel { get; set; }
+        private int LevelOfManeuverability { get; set; }
+        static int count = 0;
+        private static int GenerateRandomValue()
+        {
+            long currentTimeMillis = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond+count;
+            count++;
+            Random random = new Random((int)currentTimeMillis);
+            return random.Next(0, 101);
+        }
+        public Tank(string NameTank) 
+        {
+            this.NameTank = NameTank;
+            TankAmmunition = GenerateRandomValue();
+            ArmorLevel= GenerateRandomValue();
+            LevelOfManeuverability = GenerateRandomValue();
+        }
+        public string info(Tank tank)
+        {
+            string armorlevel = tank.ArmorLevel.ToString()+" ";
+            string levelermaner=tank.LevelOfManeuverability.ToString()+" ";
+            string tankamun= tank.ArmorLevel.ToString()+" ";
+
+            string allparametr =armorlevel+levelermaner+tankamun;
+
+            return allparametr;
+        }
+
+        public static Tank operator *(Tank tank1, Tank tank2)
+        {
+            int tank1Wins = 0;
+            int tank2Wins = 0;
+
+            if (tank1.TankAmmunition > tank2.TankAmmunition)
+                tank1Wins++;
+            else if (tank2.TankAmmunition > tank1.TankAmmunition)
+                tank2Wins++;
+
+            if (tank1.ArmorLevel > tank2.ArmorLevel)
+                tank1Wins++;
+            else if (tank2.ArmorLevel > tank1.ArmorLevel)
+                tank2Wins++;
+
+            if (tank1.LevelOfManeuverability > tank2.LevelOfManeuverability)
+                tank1Wins++;
+            else if (tank2.LevelOfManeuverability > tank1.LevelOfManeuverability)
+                tank2Wins++;
+
+            if (tank1Wins > 1)
+                return tank1;
+            else if (tank2Wins > 1)
+                return tank2;
+            else
+                return null;
+        }
+
+
+
+    }
+}
